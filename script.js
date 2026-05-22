a1ac75808fa6f53c4459114878d3d2af82516aeb
Arun <karun74315@gmail.com>
2026-05-22 09:49:55 +0530

    modal

--- script.js
+++ script.js
@@ -54,3 +54,48 @@ function addDragEventsOnColumn(column){
 addDragEventsOnColumn(todo)
 addDragEventsOnColumn(progress)
 addDragEventsOnColumn(done)
+
+const toggelModalButton = document.querySelector("#toggel-modal")
+const bg = document.querySelector(".modal .bg")
+const modal = document.querySelector(".modal")
+const addTaskButton = document.querySelector("#add-new-task")
+
+toggelModalButton.addEventListener("click", () => {
+        modal.classList.toggle("active")
+
+})
+
+bg.addEventListener("click", () => {
+        modal.classList.remove("active")
+
+})
+
+addTaskButton.addEventListener("click", ()=>{
+
+     const taskTitle = document.querySelector("#task-title-input").value
+     const taskDesc = document.querySelector("#task-desc-input").value
+
+     const div = document.createElement("div")
+     div.classList.add("task")
+     div.setAttribute("draggable", "true")
+     div.innerHTML= `<h2>${taskTitle}</h2>
+                     <p>${taskDesc}</p>
+                     <button>Delete</button>` 
+     
+     todo.appendChild(div)
+     modal.classList.remove("active")                
+
+
+    //  const template = ` <div class="task" draggable="true">
+    //                 <h2>${taskTitle}</h2>
+    //                 <p>${taskDesc}</p>
+    //                 <button>
+    //                     Delete
+    //                 </button>
+    //             </div>`
+
+    //             todo.appendChild
+
+})
+
+
